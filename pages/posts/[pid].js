226b185ba800133a66d6a9c6db9abde0c7339bbd
Krzysztof Ogaza <krzysztof.ogaza@agora.pl>
2020-06-24 17:15:56 +0200

    init

---
+++ pages/posts/[pid].js
@@ -0,0 +1,12 @@
+const { useReducer } = require("react");
+
+import { useRouter } from 'next/router'
+
+const Post = () => {
+    const router = useRouter()
+    const {pid} = router.query
+
+    return <p>Post: {pid}</p>
+}
+
+export default Post
