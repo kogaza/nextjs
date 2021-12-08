201a11a99d8e7d5f86787bac8b7d53b9e6a2ddea
Krzysiek <krzysztofogaza@gmail.com>
2021-12-08 10:16:52 +0100

    routing

---
+++ nextfire-app/pages/admin/[slug].tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function SomeAdminProfile() {
+    return (
+        <main>
+            some admin profile
+        </main>
+    )
+}
---
+++ nextfire-app/pages/admin/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AdminPostsPage() {
+    return (
+        <div>
+            Admin Posts
+        </div>
+    )
+}
