05273b47dbaa7ce7f5326487798c6cd3acaf62a6
Krzysiek <krzysztofogaza@gmail.com>
2021-12-09 14:55:12 +0100

    google login and useContext

--- nextfire-app/pages/_app.tsx
+++ nextfire-app/pages/_app.tsx
@@ -1,14 +1,15 @@
 import { Toaster } from 'react-hot-toast'
 import Navbar from '../components/Navbar'
+import { UserContext } from '../lib/context'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <>
+    <UserContext.Provider value={{ user: {}, username: 'jeff' }}>
       <Navbar />
       <Component {...pageProps} />
       <Toaster />
-    </>
+    </UserContext.Provider>
   )
 }
 
--- nextfire-app/pages/enter.tsx
+++ nextfire-app/pages/enter.tsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { UserContext } from '../lib/context';
+import { auth, googleAuthProvider } from '../lib/firebase';
 
 export default function EnterPage() {
+    const { user, username } = useContext(UserContext)
+
+    // 1. user signed out <SignInButton />
+    // 2. user signed in, but missing username <UsernameForm />
+    // 3. user signed in, has username <SignOutButton />
     return (
         <main>
-            Sing Up
+            {user ?
+                !username ? <UsernameForm /> : <SignOutButton />
+                :
+                <SignInButton />
+            }
+
         </main>
+    )
+}
+
+function SignInButton() {
+    const signInWithGoogle = async () => {
+        await auth.signInWithPopup(googleAuthProvider)
+    }
+
+    return (
+        <button className="btn-google" onClick={signInWithGoogle}>
+            <img src={'/google.png'} /> Sing in with Google
+        </button>
+    )
+}
+
+function SignOutButton() {
+    return <button onClick={() => auth.signOut()}>Sing Out</button>
+}
+
+function UsernameForm() {
+    return (
+        <>
+        </>
     )
 }
