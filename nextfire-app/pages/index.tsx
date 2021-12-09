67898bf5010924364015cb8fc73f997a97d0722a
Krzysiek <krzysztofogaza@gmail.com>
2021-12-09 13:38:37 +0100

    Toast notifications

--- nextfire-app/pages/index.tsx
+++ nextfire-app/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import toast from 'react-hot-toast'
 import Loader from '../components/Loader'
 import styles from '../styles/Home.module.css'
 
@@ -8,7 +9,15 @@ import styles from '../styles/Home.module.css'
 export default function Home() {
   return (
    <div>
-     <Loader show />
+     <button onClick={() => toast.success("hello toast!")}>
+       Toast Success
+     </button>
+     {/* <button onClick={() => toast.loading("hello toast!")}>
+       Toast Loading
+     </button> */}
+     <button onClick={() => toast.error("hello toast!")}>
+       Toast Error
+     </button>
     </div>
   )
 }
